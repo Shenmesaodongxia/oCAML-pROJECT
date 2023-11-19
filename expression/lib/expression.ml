55a3aa27f529a51a914fe18d3f41dd24a172885e
Shenmesaodongxia <112725715+Shenmesaodongxia@users.noreply.github.com>
2023-11-19 12:08:54 -0600

    Update expression.ml

--- expression/lib/expression.ml
+++ expression/lib/expression.ml
@@ -8,9 +8,16 @@ let rec string_of_expression (e:expression)
   |Identifier nm->nm
   |Application(e1,e2)->
     (string_of_expression e1) ^
-    " " ^ (string_of_expression_with_parens e2)
+    "" ^ "(" ^ (string_of_expression_with_parens e2) ^")"
+  |Equality(e1, e2) ->
+    (string_of_expression e1) ^ " = " ^ (string_of_expression e2)
 and string_of_expression_with_parens e
   = match e with 
   |Identifier nm -> nm
-  |Application _-> "(" ^ string_of_expression e ^ ")"
+  |Application _-> string_of_expression e
+  | Equality(e1, e2) -> "(" ^ (string_of_expression e1) ^ " = " ^ (string_of_expression e2) ^ ")"
+
+
     
+
+
